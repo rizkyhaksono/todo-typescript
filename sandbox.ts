488eae7949852236a4d59f7768cb9cc93170d07d
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-01-31 07:56:38 +0700

    add: objects

--- sandbox.ts
+++ sandbox.ts
@@ -7,3 +7,12 @@ const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
     console.log(input);
 });
+
+// objects in typescript
+let ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30,
+};
+
+ninja.name = "rizky";
