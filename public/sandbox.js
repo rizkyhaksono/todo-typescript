ef7d6822990c38f06eeef4919102e1b46c9c86ba
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-02-01 23:42:55 +0700

    add: function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,13 @@
 "use strict";
+const form = document.querySelector("form");
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
 // const character = "mario";
 // console.log(character);
 // const inputs = document.querySelectorAll("input");
@@ -13,12 +22,30 @@
 // };
 // ninja.name = "rizky";
 // explicit types
-let character;
-let age;
-let isLoggedIn;
-character = "rizky";
-age = 20;
-isLoggedIn = true;
-console.log(character, age, isLoggedIn);
+// let character: string;
+// let age: number;
+// let isLoggedIn: boolean;
+// character = "rizky";
+// age = 20;
+// isLoggedIn = true;
+// console.log(character, age, isLoggedIn);
 // arrays
-let ninjas = [];
+// let ninjas: string[] = [];
+// function
+// let salam: Function;
+// salam = () => {
+//     console.log("Assalamualaikum");
+// };
+// const tambah = (a: number, b: number) => {
+//     console.log("Hasilnya adalah " + (a + b));
+// };
+// tambah(5, 10);
+// it error because the return type is void
+// const minus = (a: number, b: number): void => {
+//     return a - b;
+// };
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,19 @@
+const form = document.querySelector("form")! as HTMLFormElement;
+
+const type = document.querySelector("#type")! as HTMLSelectElement;
+
+const tofrom = document.querySelector("#tofrom")! as HTMLInputElement;
+
+const details = document.querySelector("#details")! as HTMLInputElement;
+
+const amount = document.querySelector("#amount")! as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
+
 // const character = "mario";
 
 // console.log(character);
@@ -19,15 +35,40 @@
 
 // explicit types
 
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+// let character: string;
+// let age: number;
+// let isLoggedIn: boolean;
 
-character = "rizky";
-age = 20;
-isLoggedIn = true;
-console.log(character, age, isLoggedIn);
+// character = "rizky";
+// age = 20;
+// isLoggedIn = true;
+// console.log(character, age, isLoggedIn);
 
 // arrays
 
-let ninjas: string[] = [];
+// let ninjas: string[] = [];
+
+// function
+
+// let salam: Function;
+
+// salam = () => {
+//     console.log("Assalamualaikum");
+// };
+
+// const tambah = (a: number, b: number) => {
+//     console.log("Hasilnya adalah " + (a + b));
+// };
+
+// tambah(5, 10);
+
+// it error because the return type is void
+// const minus = (a: number, b: number): void => {
+//     return a - b;
+// };
+const minus = (a: number, b: number): number => {
+    return a - b;
+};
+
+let result = minus(10, 7);
+console.log(result);
