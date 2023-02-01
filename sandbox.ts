001308c1113bb7b87682e64bcce0d178acf29eb1
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-02-01 19:05:47 +0700

    explicit types

--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,28 @@
-const character = "mario";
+// const character = "mario";
 
-console.log(character);
+// console.log(character);
 
-const inputs = document.querySelectorAll("input");
+// const inputs = document.querySelectorAll("input");
 
-inputs.forEach((input) => {
-    console.log(input);
-});
+// inputs.forEach((input) => {
+//     console.log(input);
+// });
 
-// objects in typescript
-let ninja = {
-    name: "mario",
-    belt: "black",
-    age: 30,
-};
+// // objects in typescript
+// let ninja = {
+//     name: "mario",
+//     belt: "black",
+//     age: 30,
+// };
 
-ninja.name = "rizky";
+// ninja.name = "rizky";
+
+// explicit types
+
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+character = "rizky";
+age = 20;
+isLoggedIn = true;
