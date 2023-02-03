cb53429a5553589749b61b4cd6859e89cab456b2
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-02-03 14:59:42 +0700

    add: DOM & Types

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -88,3 +88,21 @@
 // };
 
 // console.log(calc(5, 10, "add"));
+
+// DOM & Type Casting
+
+// const form = document.querySelector("form")! as HTMLFormElement;
+
+// const type = document.querySelector("#type")! as HTMLSelectElement;
+
+// const tofrom = document.querySelector("#tofrom")! as HTMLInputElement;
+
+// const details = document.querySelector("#details")! as HTMLInputElement;
+
+// const amount = document.querySelector("#amount")! as HTMLInputElement;
+
+// form.addEventListener("submit", (e: Event) => {
+//     e.preventDefault();
+
+//     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+// });
