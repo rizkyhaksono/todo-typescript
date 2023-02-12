58f5421d9eb1ae48658438ef8e2c91dd11da711c
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-02-13 03:04:04 +0700

    remove: all npm packages using 'npm uninstall

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
 
-// import Button from "@mui/material/Button";
-// import { styled } from "@mui/material/styles";
-
-// const CustomButton = styled(Button)({
-//     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-//     border: 0,
-// }) as typeof Button;
-
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("rizky", "work on the natee website", 300);
 
